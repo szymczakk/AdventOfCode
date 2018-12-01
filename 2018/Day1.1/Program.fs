62a725bd8c7918efbae69684f8101c71b5ee55b7
szymczakk <rakaz666@gmail.com>
2018-12-01 21:49:12 +0100

    Day one, first star

---
+++ 2018/Day1.1/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let negate x = x * -1 
+
+let calculate (n:string) =
+    match n.[0] with 
+    | '+' -> 0 + (n |> string |> int)
+    | '-' -> 0 - (n |> string |> int |> negate)
+
+let parse (x : string[]) = 
+    x|> Array.map(calculate)
+     |> Array.sum
+
+[<EntryPoint>]
+let main argv =
+    let readLines = System.IO.File.ReadAllLines("input.txt")
+    let result = parse(readLines)
+
+    Console.WriteLine(result)
+    0
