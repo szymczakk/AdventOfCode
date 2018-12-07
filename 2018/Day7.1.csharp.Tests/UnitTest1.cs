844aac7b68386ace1a6fc88edcf9264bb73ce104
szymczakk <rakaz666@gmail.com>
2018-12-07 11:33:33 +0100

    Day 7.1 - initial

---
+++ 2018/Day7.1.csharp.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day7._1.csharp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly string[] _testCase = new[]
+        {
+            "Step C must be finished before step A can begin.",
+            "Step C must be finished before step F can begin.",
+            "Step A must be finished before step B can begin.",
+            "Step A must be finished before step D can begin.",
+            "Step B must be finished before step E can begin.",
+            "Step D must be finished before step E can begin.",
+            "Step F must be finished before step E can begin."
+        };
+
+        [TestMethod]
+        public void TestParseOrderMethod()
+        {
+            var w = new Worker();
+            var result = w.OrderInstruction(_testCase);
+            Assert.AreEqual("CABDFE", _testCase);
+        }
+
+        [TestMethod]
+        public void TestParseInput()
+        {
+            var w = new Worker();
+            var result = w.ParseInput(_testCase);
+
+            Assert.IsTrue(result.First().Value.Contains("F"));
+            Assert.IsTrue(result.First().Value.Contains("A"));
+            Assert.AreEqual(2, result.First().Value.Count);
+        }
+    }
+}
---
+++ 2018/Day7.1.csharp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Xml;
+
+namespace Day7._1.csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Worker
+    {
+        public string OrderInstruction(string[] input)
+        {
+            var parsedAndOrdered = ParseInput(input);
+
+            var linkedList = new LinkedList<string>();
+
+            linkedList.AddFirst(parsedAndOrdered.First().Key);
+
+            foreach (var rowInDict in parsedAndOrdered)
+            {
+                foreach (var taskToComplete in rowInDict.Value)
+                {
+                    var beforeNode = linkedList.Find(rowInDict.Key);
+
+                    linkedList.AddAfter(beforeNode, taskToComplete.Value);
+                }
+            }
+
+
+            return "";
+        }
+
+        public Dictionary<string, SortedList<string, string>> ParseInput(string[] input)
+        {
+            var result = new Dictionary<string, SortedList<string, string>>();
+
+            foreach (var row in input)
+            {
+                var arr = row.Split(' ');
+                var before = arr[1];
+                var after = arr[7];
+
+                if (result.ContainsKey(before))
+                {
+                    result[before].Add(after, after);
+                }
+                else
+                {
+                    result.Add(before, new SortedList<string, string>(){{after, after}});
+                }
+            }
+
+            return result;
+        }
+    }
+}
