a7d37744d6d0e8bc1db5a9df5d7ff283a1d23ed0
szymczakk <rakaz666@gmail.com>
2018-12-13 09:50:53 +0100

    Day 12.1 - change PC

--- 2018/Day12.1.csharp.Tests/UnitTest1.cs
+++ 2018/Day12.1.csharp.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Reflection.Metadata;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Day12._1.csharp.Tests
@@ -24,13 +26,64 @@ namespace Day12._1.csharp.Tests
             "###.# => #",
             "####. => #"
         };
+
         [TestMethod]
         public void TestCalculateAmoutnOfPlantsAfterGeneration()
         {
             var w = new W();
-            var result = w.CalculateAmoutnOfPlantsAfterGeneration(_testCase, 20);
+            var parsed = w.ParseInput(_testCase);
+            var result = w.CalculateAmoutnOfPlantsAfterGeneration(parsed.Item1, parsed.Item2, 20);
 
             Assert.AreEqual(325, result);
+        }
+
+        [TestMethod]
+        public void TestParseInputInitialState()
+        {
+            var w = new W();
+            
+            var parsed = w.ParseInput(_testCase);
+
+            Assert.AreEqual(75, parsed.Item1.Length);
+
+            Assert.AreEqual(1, parsed.Item1[parsed.Item1.Length / 3 + 0]);
+            Assert.AreEqual(0, parsed.Item1[parsed.Item1.Length / 3 + 1]);
+            Assert.AreEqual(0, parsed.Item1[parsed.Item1.Length / 3 + 2]);
+            Assert.AreEqual(1, parsed.Item1[parsed.Item1.Length / 3 + 3]);
+            Assert.AreEqual(0, parsed.Item1[parsed.Item1.Length / 3 + 4]);
+            Assert.AreEqual(1, parsed.Item1[parsed.Item1.Length / 3 + 24]);
+
+        }
+
+        [TestMethod]
+        public void TestParseInputRules()
+        {
+            var w = new W();
+
+            var parsed = w.ParseInput(_testCase);
+
+            Assert.AreEqual(14, parsed.Item2.Length);
+
+            Assert.AreEqual("00011", string.Join("", parsed.Item2[0].Sequence));
+            Assert.AreEqual(1, parsed.Item2[0].Result);
+            Assert.AreEqual("11110", string.Join("", parsed.Item2[13].Sequence));
+            Assert.AreEqual(1, parsed.Item2[13].Result);
+        }
+
+        [TestMethod]
+        public void TestGetNextGeneration()
+        {
+            var w = new W();
+            var parsed = w.ParseInput(_testCase);
+            var result = w.GetNextGeneration(parsed.Item1, parsed.Item2);
+            
+            Assert.AreEqual(1, result[25]);
+            Assert.AreEqual(1, result[29]);
+            Assert.AreEqual(1, result[34]);
+            Assert.AreEqual(1, result[40]);
+            Assert.AreEqual(1, result[43]);
+
+            Assert.AreEqual("0001000100001000001001001001", string.Join("", result.Skip(22).Take(28)));
         }
     }
 }
--- 2018/Day12.1.csharp/Program.cs
+++ 2018/Day12.1.csharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
 
 namespace Day12._1.csharp
 {
@@ -6,15 +9,130 @@ namespace Day12._1.csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = System.IO.File.ReadAllLines("input.txt");
+            var w = new W();
+            var parsed = w.ParseInput(input);
+            var result = w.CalculateAmoutnOfPlantsAfterGeneration(parsed.Item1, parsed.Item2, 20);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 
     public class W
     {
-        public int CalculateAmoutnOfPlantsAfterGeneration(string[] input, int generationNo)
+        public int CalculateAmoutnOfPlantsAfterGeneration(int[] initialState, Rule[] rules, int generationNo)
         {
-            return 0;
+            var results = new List<int[]>(generationNo)
+            {
+                initialState
+            };
+
+            for (int i = 1; i < generationNo + 1; i++)
+            {
+                results.Add(GetNextGeneration(results[i - 1], rules));
+            }
+
+            var sum = 0;
+
+            foreach (var generationLine in results)
+            {
+                sum += generationLine.Sum();
+            }
+
+            return sum;
         }
+
+        public (int[] initialState, Rule[] rules) ParseInput(string[] input)
+        {
+            var initialState = ParseInitialState(input[0]);
+            var rules = ParseRules(input.Skip(2));
+            return (initialState, rules );
+        }
+
+        public int[] GetNextGeneration(int[] state, Rule[] rules)
+        {
+            var result = new List<int>
+            {
+                state[0], state[1]
+            };
+
+            for (var i = 0; i < state.Length; i++)
+            {
+                var toApply = true;
+                foreach (var rule in rules)
+                {
+                    toApply = true;
+                    for (var j = 0; j < rule.Sequence.Length; j++)
+                    {
+                        try
+                        {
+                            if (state[j + i] != rule.Sequence[j])
+                            {
+                                toApply = false;
+                                break;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            toApply = false;
+                            break;
+                        }
+                    }
+                    if (toApply)
+                    {
+                        result.Add(rule.Result);
+                        break;
+                    }
+                }
+                if (!toApply)
+                {
+                    result.Add(0);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private Rule[] ParseRules(IEnumerable<string> input)
+        {
+            var result = new List<Rule>();
+
+            foreach (var rule in input)
+            {
+                var parsedRule = new Rule();
+                for (var i = 0; i < 5; i++)
+                {
+                    parsedRule.Sequence[i] = rule[i] == '.' ? 0 : 1;
+                }
+                parsedRule.Result = rule.Last() == '.' ? 0 : 1;
+                result.Add(parsedRule);
+            }
+
+            return result.ToArray();
+        }
+
+        private int[] ParseInitialState(string input)
+        {
+            var plants = input.Split(" ")[2];
+
+            var result = new int[plants.Length * 3];
+
+            for (var i = 0; i < plants.Length; i++)
+            {
+                result[i + plants.Length] = plants[i] == '.' ? 0 : 1;
+            }
+
+            return result;
+        }
+    }
+
+    public class Rule
+    {
+        public Rule()
+        {
+            Sequence = new int[5];
+        }
+        public int[] Sequence { get; set; }
+        public int Result { get; set; }
     }
 }
