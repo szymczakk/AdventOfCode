b68a9fb435f2ad54cb4f089e10256535d050ae3e
szymczakk <rakaz666@gmail.com>
2018-12-15 23:17:06 +0100

    Day 14.2

--- 2018/Day14.1.csharp.Tests/UnitTest1.cs
+++ 2018/Day14.1.csharp.Tests/UnitTest1.cs
@@ -5,15 +5,26 @@ namespace Day14._1.csharp.Tests
     [TestClass]
     public class UnitTest1
     {
-        private readonly int[] _initialState = {3, 7};
+        private readonly int[] _initialState = { 3, 7 };
 
         [TestMethod]
         public void TestGetScoresAfterRecipe()
         {
             var w = new W();
-            var result = w.GetScoresAfterRecipe(_initialState, 9);
-            
-            Assert.AreEqual(new[]{ 5, 9, 4,1 ,4 , 2, 9, 8, 8, 2 }, result);
+            CollectionAssert.AreEqual(new int[] { 5, 1, 5, 8, 9, 1, 6, 7, 7, 9 }, w.GetScoresAfterRecipe(_initialState, 9));
+            CollectionAssert.AreEqual(new int[] { 0, 1, 2, 4, 5, 1, 5, 8, 9, 1 }, w.GetScoresAfterRecipe(_initialState, 5));
+            CollectionAssert.AreEqual(new int[] { 9, 2, 5, 1, 0, 7, 1, 0, 8, 5 }, w.GetScoresAfterRecipe(_initialState, 18));
+            CollectionAssert.AreEqual(new int[] { 5, 9, 4, 1, 4, 2, 9, 8, 8, 2 }, w.GetScoresAfterRecipe(_initialState, 2018));
+        }
+
+        [TestMethod]
+        public void TestGetLeftMostAmoutBeforeValue()
+        {
+            var w = new W();
+            Assert.AreEqual(9, w.GetLeftMostAmoutBeforeValue(_initialState, new[] { 5, 1, 5, 8, 9 }));
+            Assert.AreEqual(5, w.GetLeftMostAmoutBeforeValue(_initialState, new[] { 0, 1, 2, 4, 5 }));
+            Assert.AreEqual(18, w.GetLeftMostAmoutBeforeValue(_initialState, new[] { 9, 2, 5, 1, 0 }));
+            Assert.AreEqual(2018, w.GetLeftMostAmoutBeforeValue(_initialState, new[] { 5, 9, 4, 1, 4 }));
         }
     }
 }
--- 2018/Day14.1.csharp/Program.cs
+++ 2018/Day14.1.csharp/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day14._1.csharp
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly int[] _initialState = { 3, 7 };
+
+        private static readonly int[] _task2WrongAnswers = { 793061, 141045485, 141045484, 141045483, 20276283 };
+
+        private static void Main(string[] args)
         {
             var w = new W();
-            var result = w.GetScoresAfterRecipe(new[] {3, 7}, 793061);
+            var result = w.GetScoresAfterRecipe(_initialState, 793061);
 
-            Console.WriteLine(result);
+            Console.WriteLine(string.Join("", result));
+
+            var result2 = w.GetLeftMostAmoutBeforeValue(_initialState, new[] { 7, 9, 3, 0, 6, 1 });
+            if (_task2WrongAnswers.Contains(result2))
+            {
+                return;
+            }
+            Console.WriteLine(result2);
         }
     }
 
@@ -17,8 +30,92 @@ namespace Day14._1.csharp
     {
         public int[] GetScoresAfterRecipe(int[] initalState, int recipeNo)
         {
-            var result = new int[10];
-            return result;
+            var scoreBoard = new List<int>(initalState);
+
+            var elf1Index = 0;
+            var elf2Index = 1;
+
+            while (scoreBoard.Count <= recipeNo + 10)
+            {
+                var newRecipe = scoreBoard[elf1Index] + scoreBoard[elf2Index];
+                if (newRecipe > 9)
+                {
+                    scoreBoard.Add((int)(newRecipe / 10));
+                    scoreBoard.Add(newRecipe % 10);
+                }
+                else
+                {
+                    scoreBoard.Add(newRecipe);
+                }
+
+                elf1Index = (elf1Index + (scoreBoard[elf1Index] + 1) % scoreBoard.Count) % scoreBoard.Count;
+                elf2Index = (elf2Index + (scoreBoard[elf2Index] + 1) % scoreBoard.Count) % scoreBoard.Count;
+            }
+
+            return scoreBoard.Skip(recipeNo).Take(10).ToArray();
+        }
+
+        public int GetLeftMostAmoutBeforeValue(int[] initialState, int[] value)
+        {
+            var scoreBoard = new List<int>(initialState);
+            var elf1Index = 0;
+            var elf2Index = 1;
+
+            bool found = false;
+            var intToSubstitute = 1;
+
+            while (!found)
+            {
+                var newRecipe = scoreBoard[elf1Index] + scoreBoard[elf2Index];
+                if (newRecipe > 9)
+                {
+                    scoreBoard.Add((int)(newRecipe / 10));
+                    scoreBoard.Add(newRecipe % 10);
+                }
+                else
+                {
+                    scoreBoard.Add(newRecipe);
+                }
+
+                elf1Index = (elf1Index + (scoreBoard[elf1Index] + 1) % scoreBoard.Count) % scoreBoard.Count;
+                elf2Index = (elf2Index + (scoreBoard[elf2Index] + 1) % scoreBoard.Count) % scoreBoard.Count;
+
+                if (scoreBoard.Count < value.Length)
+                {
+                    continue;
+                }
+
+
+                if (scoreBoard[scoreBoard.Count - 1] == value[value.Length - 1])
+                {
+                    found = true;
+                    for (var i = 0; i < value.Count(); i++)
+                    {
+                        intToSubstitute = 0;
+                        if (scoreBoard[scoreBoard.Count - i - 1] != value[value.Length - i - 1])
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (scoreBoard[scoreBoard.Count - 2] == value[value.Length - 1])
+                {
+                    found = true;
+                    for (var i = 0; i < value.Count(); i++)
+                    {
+                        intToSubstitute = 1;
+                        if (scoreBoard[scoreBoard.Count - i - 2] != value[value.Length - i - 1])
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return scoreBoard.Count - value.Length - intToSubstitute;
         }
     }
 }
