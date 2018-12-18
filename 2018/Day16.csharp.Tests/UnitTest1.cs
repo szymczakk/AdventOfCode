8e3a00f45de6cba904e5f1beaff229a9624aefdd
szymczakk <rakaz666@gmail.com>
2018-12-18 12:42:32 +0100

    Day 16 - additional tests

--- 2018/Day16.csharp.Tests/UnitTest1.cs
+++ 2018/Day16.csharp.Tests/UnitTest1.cs
@@ -10,11 +10,35 @@ namespace Day16.csharp.Tests
         private readonly string _action = "9 2 1 2";
 
         [TestMethod]
-        public void TestCanBeCalculatedBy3OrMoreInstruction()
+        public void TestCanBeCalculatedBy3OrMoreInstruction1()
         {
             var w = new W();
 
             Assert.IsTrue(w.CanBeCalculatedByXOrMoreInstruction(3, _before, _after, _action));
+        }
+
+        [TestMethod]
+        public void TestCanBeCalculatedBy3OrMoreInstruction2()
+        {
+            var w = new W();
+
+            Assert.IsTrue(w.CanBeCalculatedByXOrMoreInstruction(3, "3, 2, 1, 1", "3, 2, 11, 1", "9 2 10 2"));
+        }
+
+        [TestMethod]
+        public void TestCanBeCalculatedBy3OrMoreInstruction3()
+        {
+            var w = new W();
+
+            Assert.IsTrue(w.CanBeCalculatedByXOrMoreInstruction(3, "3, 2, 1, 1", "3, 2, 13, 1", "9 2 13 2"));
+        }
+
+        [TestMethod]
+        public void TestCanBeCalculatedBy3OrMoreInstruction4()
+        {
+            var w = new W();
+
+            Assert.IsTrue(w.CanBeCalculatedByXOrMoreInstruction(3, "3, 2, 1, 1", "3, 2, 1, 23", "9 0 20 3"));
         }
     }
 }
